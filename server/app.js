27f4b9efba0f6a8d65e245bf4fcc100922e42d4d
George Merida <gmerida92@gmail.com>
2022-06-01 20:13:55 -0700

    first commit

---
+++ server/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+
+
+
+app.get('/goodbye/forever', (req, res) => {
+    res.send("So long. Farewell. Have a great life!");
+});
+
+app.get('/goodbye/until/:time', (req, res) => {
+    res.send(`Have a nice day! See you ${req.params.time}.`);
+});
+
+
+app.get(['/goodbye', '/goodbye/*'], (req, res) => {
+    res.send("Goodbye, my friend!");
+});
+
+
+app.get('/hello', (req, res) => {
+    res.send("Hello, my friend!");
+});
+
+app.get('/', (req, res) => {
+    res.send("Server is alive");
+});
+
+const port = 5000;
+app.listen(port, () => console.log('Server is listening on port', port));
